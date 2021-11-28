44b220735782e6f018821322e6a9dc6f03010f9d
Believer Just <617146817@qq.com>
2021-11-28 15:44:55 +0800

    change：增加物理仿真器

--- 3DTest/YXBoxFloatAnimationVC/YXBoxFloatAnimationVC.m
+++ 3DTest/YXBoxFloatAnimationVC/YXBoxFloatAnimationVC.m
@@ -27,6 +27,13 @@
 @property (nonatomic, strong) UIImageView *topImgV;
 @property (nonatomic, strong) UIImageView *bottomImgV;
 
+@property (nonatomic, strong) UIDynamicAnimator *dynamicAnimator; //物理仿真器
+@property (nonatomic, strong) UICollisionBehavior *collision; //碰撞行为
+@property (nonatomic, strong) UIGravityBehavior *gravity; //重力行为
+@property (nonatomic, strong) UIDynamicItemBehavior *itemBehavior; //弹性行为
+@property (nonatomic, strong) UIPushBehavior *pushBehavior; //推力行为
+@property (nonatomic, strong) UIImageView *projImgVFirst;
+
 @end
 
 @implementation YXBoxFloatAnimationVC
@@ -48,12 +55,13 @@
     
     [self initOutView:0.5];
     
+    [self initBehavior];
+    
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
     [btn setTitle:@"动画" forState:UIControlStateNormal];
     btn.frame = CGRectMake(100, 100, 100, 30);
     [btn addTarget:self action:@selector(progressBtn:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn];
-    [self initTitleLab];
 }
 
 #pragma mark - progress
@@ -231,6 +239,55 @@
     finished(rotateValue, boolRotateValue);
 }
 
+#pragma mark - 设定碰撞范围
+- (void)diyBoundaryLine {
+    
+    CGFloat endFloat = self.containerView.bounds.size.width;
+    CGPoint startF = CGPointMake(0, 0);
+    CGPoint endF = CGPointMake(endFloat, 0);
+    [self.collision addBoundaryWithIdentifier:@"lineF" fromPoint:startF toPoint:endF];
+    
+    CGFloat endHFloat = self.containerView.bounds.size.height;
+    CGPoint startS = CGPointMake(0, endHFloat);
+    CGPoint endS = CGPointMake(endFloat, endHFloat);
+    [self.collision addBoundaryWithIdentifier:@"lineS" fromPoint:startS toPoint:endS];
+}
+
+#pragma mark - 取出范围内的随机数
+- (int)getRandomNumber:(int)from to:(int)to {
+    
+    return (int)(from + (arc4random() % (to - from + 1)));
+}
+
+#pragma mark - 初始化物理仿真器
+- (void)initBehavior {
+    
+    _projImgVFirst = [[UIImageView alloc] initWithFrame:CGRectMake(_width / 2, _height - 30, 20, 20)];
+    _projImgVFirst.layer.transform = CATransform3DTranslate(_projImgVFirst.layer.transform, 0, 0, 0);
+    [_projImgVFirst setImage:[UIImage imageNamed:@"BoxProjImgFirst"]];
+    [_containerView addSubview:_projImgVFirst];
+    
+    [self.gravity addItem:_projImgVFirst];
+    [self.collision addItem:_projImgVFirst];
+    
+    self.itemBehavior = [[UIDynamicItemBehavior alloc] initWithItems:@[_projImgVFirst]]; //将所有物理元素添加动力元素行为
+    self.itemBehavior.elasticity = 0.9; //设置弹性越大弹的越猛（1是原来的力气反弹，比1大会弹回去加力，比1小会衰减）
+    self.itemBehavior.friction = 0; //磨擦力
+    self.itemBehavior.density = 0; //密度，密度*体积等于质量 物理元素越大密度越大，越难推动
+    self.itemBehavior.resistance = 0; //抗阻力 0~CGFLOAT_MAX ，阻碍原有所加注的行为（如本来是重力自由落体行为，则阻碍其下落，阻碍程度根据其值来决定）
+    self.itemBehavior.allowsRotation = NO; //是否允许旋转
+    
+    self.pushBehavior = [[UIPushBehavior alloc] initWithItems:@[_projImgVFirst] mode:UIPushBehaviorModeInstantaneous]; //初始化推动行为
+    self.pushBehavior.active = YES; //激活力的作用
+    self.pushBehavior.magnitude = 1; //加速度，数值越大力越大
+    self.pushBehavior.angle = kRadiasToDegrees(30); //力方向
+    
+    [self.dynamicAnimator addBehavior:self.gravity];
+    [self.dynamicAnimator addBehavior:self.collision];
+    [self.dynamicAnimator addBehavior:self.itemBehavior];
+//    [self.dynamicAnimator addBehavior:self.pushBehavior];
+}
+
 #pragma mark - 初始化外围放大视图
 - (void)initOutView:(CGFloat)value {
     
@@ -264,98 +321,47 @@
     _bottomImgV.layer.transform = CATransform3DTranslate(_bottomImgV.layer.transform, 0, 0, value + addValue);
 }
 
-- (void)initTitleLab {
-    
-    UILabel *frontLab = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 60, 20)];
-    frontLab.center = self.frontImgV.center;
-    frontLab.font = [UIFont systemFontOfSize:14];
-    frontLab.textColor = [UIColor redColor];
-    frontLab.textAlignment = NSTextAlignmentCenter;
-    frontLab.text = @"front";
-    [self.frontImgV addSubview:frontLab];
-    
-    UILabel *backLab = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 60, 20)];
-    backLab.center = self.backImgV.center;
-    backLab.font = [UIFont systemFontOfSize:14];
-    backLab.textColor = [UIColor redColor];
-    backLab.textAlignment = NSTextAlignmentCenter;
-    backLab.text = @"back";
-    [self.backImgV addSubview:backLab];
-    
-    UILabel *leftLab = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 60, 20)];
-    leftLab.center = self.leftImgV.center;
-    leftLab.font = [UIFont systemFontOfSize:14];
-    leftLab.textColor = [UIColor redColor];
-    leftLab.textAlignment = NSTextAlignmentCenter;
-    leftLab.text = @"left";
-    [self.leftImgV addSubview:leftLab];
-    
-    UILabel *rightLab = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 60, 20)];
-    rightLab.center = self.rightImgV.center;
-    rightLab.font = [UIFont systemFontOfSize:14];
-    rightLab.textColor = [UIColor redColor];
-    rightLab.textAlignment = NSTextAlignmentCenter;
-    rightLab.text = @"right";
-    [self.rightImgV addSubview:rightLab];
-    
-    UILabel *topLab = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 60, 20)];
-    topLab.center = self.topImgV.center;
-    topLab.font = [UIFont systemFontOfSize:14];
-    topLab.textColor = [UIColor redColor];
-    topLab.textAlignment = NSTextAlignmentCenter;
-    topLab.text = @"top";
-    [self.topImgV addSubview:topLab];
-    
-    UILabel *bottomLab = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 60, 20)];
-    bottomLab.center = self.bottomImgV.center;
-    bottomLab.font = [UIFont systemFontOfSize:14];
-    bottomLab.textColor = [UIColor redColor];
-    bottomLab.textAlignment = NSTextAlignmentCenter;
-    bottomLab.text = @"bottom";
-    [self.bottomImgV addSubview:bottomLab];
-}
-
 #pragma mark - 初始化视图
 - (void)initView {
     
     //前
     _frontImgV = [[UIImageView alloc] initWithFrame:self.containerView.bounds];
-    _frontImgV.backgroundColor = [[UIColor blueColor] colorWithAlphaComponent:1];
     _frontImgV.layer.transform = CATransform3DTranslate(_frontImgV.layer.transform, 0, 0, (_height / 2));
+    [_frontImgV setImage:[UIImage imageNamed:@"YXBlindBoxFrontImgFirst"]];
     [self.containerView addSubview:_frontImgV];
     
     //后
     _backImgV = [[UIImageView alloc] initWithFrame:self.containerView.bounds];
-    _backImgV.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:1];
     _backImgV.layer.transform = CATransform3DTranslate(_backImgV.layer.transform, 0, 0, -(_height / 2));
+    [_backImgV setImage:[UIImage imageNamed:@"YXBlindBoxBackImgFirst"]];
     [self.containerView addSubview:_backImgV];
     
     //左
     _leftImgV = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, _height, _height)];
-    _leftImgV.backgroundColor = [[UIColor yellowColor] colorWithAlphaComponent:1];
     _leftImgV.layer.transform = CATransform3DTranslate(_leftImgV.layer.transform, -(_height / 2), 0, 0);
     _leftImgV.layer.transform = CATransform3DRotate(_leftImgV.layer.transform, M_PI_2, 0, 1, 0);
+    [_leftImgV setImage:[UIImage imageNamed:@"YXBlindBoxLeftImgFirst"]];
     [self.containerView addSubview:_leftImgV];
     
     //右
     _rightImgV = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, _height, _height)];
-    _rightImgV.backgroundColor = [[UIColor purpleColor] colorWithAlphaComponent:1];
     _rightImgV.layer.transform = CATransform3DTranslate(_rightImgV.layer.transform, (_width * 2 - _height) / 2, 0, 0);
     _rightImgV.layer.transform = CATransform3DRotate(_rightImgV.layer.transform, M_PI_2, 0, 1, 0);
+    [_rightImgV setImage:[UIImage imageNamed:@"YXBlindBoxRightImgFirst"]];
     [self.containerView addSubview:_rightImgV];
     
     //上
     _topImgV = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, _width, _height)];
-    _topImgV.backgroundColor = [[UIColor orangeColor] colorWithAlphaComponent:1];
     _topImgV.layer.transform = CATransform3DTranslate(_topImgV.layer.transform, 0, -(_height / 2), 0);
     _topImgV.layer.transform = CATransform3DRotate(_topImgV.layer.transform, M_PI_2, 1, 0, 0);
+    [_topImgV setImage:[UIImage imageNamed:@"YXBlindBoxTopImgFirst"]];
     [self.containerView addSubview:_topImgV];
     
     //下
     _bottomImgV = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, _width, _height)];
-    _bottomImgV.backgroundColor = [[UIColor greenColor] colorWithAlphaComponent:1];
     _bottomImgV.layer.transform = CATransform3DTranslate(_bottomImgV.layer.transform, 0, (_height / 2), 0);
     _bottomImgV.layer.transform = CATransform3DRotate(_bottomImgV.layer.transform, M_PI_2, -1, 0, 0);
+    [_bottomImgV setImage:[UIImage imageNamed:@"YXBlindBoxBottomImgFirst"]];
     [self.containerView addSubview:_bottomImgV];
 }
 
@@ -369,7 +375,34 @@
     }
     return _containerView;
 }
-
+/** 创建物理仿真器 */
+- (UIDynamicAnimator *)dynamicAnimator {
+    
+    if (!_dynamicAnimator) {
+        _dynamicAnimator = [[UIDynamicAnimator alloc] initWithReferenceView:self.containerView];
+    }
+    return _dynamicAnimator;
+}
+/** 碰撞初始化 */
+- (UICollisionBehavior *)collision {
+    
+    if (!_collision) {
+        _collision = [[UICollisionBehavior alloc] init];
+        _collision.translatesReferenceBoundsIntoBoundary = YES; //以参照视图作为碰撞行为的边界
+        _collision.collisionMode = UICollisionBehaviorModeEverything; //碰撞参照
+        [self diyBoundaryLine];
+    }
+    return _collision;
+}
+/** 重力初始化 */
+- (UIGravityBehavior *)gravity {
+    
+    if (!_gravity) {
+        _gravity = [[UIGravityBehavior alloc] init];
+        _gravity.magnitude = 1; //重力大小
+    }
+    return _gravity;
+}
 
 #pragma mark - 其它
 - (void)squareMethod {
